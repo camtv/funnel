3a200d0fbd33e04720f9ff078ef42cb000747851
Prez Ivan <ivan.camtv@gmail.com>
2020-04-28 20:49:46 +0200

    oto1

--- all/assets/js/conf.js
+++ all/assets/js/conf.js
@@ -2,18 +2,26 @@
 var host = window.location.hostname;
 
 var currrentLocation = "https://www.internetcreaindipendenza.it";
-if (host.indexOf("github") != -1)
+var toUIOto1 = 743000;
+var toUIOto2 = 508000;
+if (host.indexOf("github") != -1) {
     currrentLocation = "https://camtv.github.io/funnel/all/build";
-else if (host.indexOf("127.0.0.1") != -1 || host.indexOf("localhost") != -1)
-    currrentLocation = location.protocol + '//' + location.hostname +":"+ location.port;
+    toUIOto1 = 12000;
+    toUIOto2 = 12000;
+}
+else if (host.indexOf("127.0.0.1") != -1 || host.indexOf("localhost") != -1) {
+    currrentLocation = location.protocol + '//' + location.hostname + ":" + location.port;
+    toUIOto1 = 5000;
+    toUIOto2 = 5000;
+}
 
 
 var Sets = {
     CamTVServer: 'https://www.testcam.tv',
     OTO1: currrentLocation+'/oto1.html',
-    OTO1_PurchaseUI_Timeout: 10000, //743000,
+    OTO1_PurchaseUI_Timeout: toUIOto1,
     OTO2: currrentLocation+'/oto2.html',
-    OTO2_PurchaseUI_Timeout: 508000,
+    OTO2_PurchaseUI_Timeout: toUIOto2,
     ThankYou: currrentLocation+'/thankyou.html'
 }
 
