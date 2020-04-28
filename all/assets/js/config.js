c10eccc1158d96b1e05b325bfacdffd59f8ce581
Prez Ivan <ivan.camtv@gmail.com>
2020-04-28 20:21:15 +0200

    build

--- all/assets/js/config.js
+++ all/assets/js/config.js
@@ -1,11 +1,11 @@
 
 var Sets = {
     CamTVServer: 'https://www.testcam.tv',
-    OTO1: '/oto1.html',
+    OTO1: 'https://camtv.github.io/funnel/all/build/oto1.html',
     OTO1_PurchaseUI_Timeout: 10000, //743000,
-    OTO2: '/oto2.html',
+    OTO2: 'https://camtv.github.io/funnel/all/build/oto2.html',
     OTO2_PurchaseUI_Timeout: 508000,
-    ThankYou: '/thankyou'
+    ThankYou: 'https://camtv.github.io/funnel/all/build/thankyou'
 }
 
 export default Sets;
