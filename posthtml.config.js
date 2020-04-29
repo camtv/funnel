8c696c7db9dd181c2537300b1cebe32faacd5978
Prez Ivan <ivan.camtv@gmail.com>
2020-04-29 17:10:46 +0200

    ogimage e gestioen errori payment oto2

--- posthtml.config.js
+++ posthtml.config.js
@@ -2,7 +2,8 @@ module.exports = {
   plugins: {
     "posthtml-expressions": {
       locals: {
-        CAMTV_SERVER: process.env.CAMTV_SERVER
+        CAMTV_SERVER: process.env.CAMTV_SERVER,
+        DOMAIN_NAME_WITH_PROTOCOL: process.env.DOMANI_NAME_WITH_PROTOCOL,
       }
     }
   }
