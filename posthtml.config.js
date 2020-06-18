ef1c20c137b726ab78e42168916ebadb182bc878
Prez Ivan <ivpcode@gmail.com>
2020-06-18 11:52:30 +0200

    offer centrale

--- posthtml.config.js
+++ posthtml.config.js
@@ -5,6 +5,9 @@ module.exports = {
         CAMTV_SERVER: process.env.CAMTV_SERVER,
         DOMAIN_NAME_WITH_PROTOCOL: process.env.DOMANI_NAME_WITH_PROTOCOL,
       }
+    },
+    'posthtml-include': {
+      root: __dirname,
     }
   }
 };
