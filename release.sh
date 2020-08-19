c6550e2d25bcb2c98c20f241b7f27457307d2b2c
Prez Ivan <ivpcode@gmail.com>
2020-08-19 19:47:23 +0200

    index_fb2

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 export CAMTV_SERVER=https://www.testcam.tv
 export DOMAIN_NAME_WITH_PROTOCOL=https://camtv.github.io/
-parcel build --out-dir build --public-url https://camtv.github.io/funnel/build/ --no-cache index.html body.html oto1.html oto2.html  thankyou.html index_fb.html body_fb.html form_bump2_more.html
+parcel build --out-dir build --public-url https://camtv.github.io/funnel/build/ --no-cache index.html body.html oto1.html oto2.html  thankyou.html index_fb.html index_fb2.html body_fb.html form_bump2_more.html
 
